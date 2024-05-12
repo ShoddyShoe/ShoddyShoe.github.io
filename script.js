60c91d3feebfc48943f8289309675b6cf250edd8
Shoddy <ShoddyShoeStoneworks@gmail.com>
2024-05-11 21:03:16 -0600

    Added Coming Soon Hover Text

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const menu = document.getElementById("menu");
 
+function delay(milliseconds){
+  return new Promise(resolve => {
+      setTimeout(resolve, milliseconds);
+  });
+}
+
 Array.from(document.getElementsByClassName("menu-item"))
   .forEach((item, index) => {
     item.onmouseover = () => {
