0daffd934ebcb4afd221e119311362f1f2969ecf
Shoddy <ShoddyShoeStoneworks@gmail.com>
2024-05-31 23:03:13 -0600

    Finished About Page And Polished Effects

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menu = document.getElementById("menu");
-var aboutItems = Array.from(document.getElementsByClassName('Avatar'));
+var aboutItems = Array.from(document.getElementsByClassName('card'));
 const menuItems = document.getElementById('menu-items');
+const aboutPageBackButton = document.getElementById("aboutPageBackButton");
 
 function delay(milliseconds){
   return new Promise(resolve => {
@@ -16,6 +17,9 @@ Array.from(document.getElementsByClassName("menu-item"))
   });
 
 async function aboutPageTransition(){
+  document.querySelectorAll('.cardTitle').forEach(t => {
+    t.classList.remove('expanded', 'glass');
+  });
   menuItems.classList.remove('bounceBack-animation');
   menuItems.classList.add('bounce-animation');
   await delay(1000);
@@ -23,33 +27,53 @@ async function aboutPageTransition(){
     element.classList.remove('glideBack-animation')
     element.classList.add('glide-animation');
     await delay(250);
-  document.getElementById("arrow").classList.remove("arrowBack-animation")
-  document.getElementById("arrow").classList.add("arrow-animation")
   }
+  await delay(1200);
+  aboutPageBackButton.classList.remove('arrowBack-animation')
+  aboutPageBackButton.classList.add('arrow-animation')
 }
 
 var cards = document.querySelectorAll('.card');
 
 cards.forEach(function(card) {
-  card.addEventListener('click', function() {
-      cards.forEach(function(c) {
-      var character = c.querySelector('.character');
-      character.style.top = "15%"
-      c.style.height = "250px"
-      c.style.bottom = "-150px"
+  card.addEventListener('click', async function(event) {
+    if (event.target.matches('.innerCard') & !event.target.closest('.card').classList.contains("clicked")) {
+    var title = this.querySelector('.cardTitle');
+    if (event.target.closest('.card') !== this) return;
+    cards.forEach(function(c) {
       c.classList.remove('clicked');
-      });
-      var character = this.querySelector('.character');
-      character.style.top = "2%";
-      this.style.height = "400px";
-      this.style.bottom = "0px";
-      this.classList.add('clicked');
+      c.classList.add('wait');
+      var character = c.querySelector('.character');
+      character.style.removeProperty('top');
+      c.style.removeProperty('height');
+      c.style.removeProperty('bottom');
+    });
+    this.classList.add('clicked');
+    document.querySelectorAll('.cardTitle').forEach(t => {
+      t.style.opacity = "0"
+    });
+    await delay(500)
+    var character = this.querySelector('.character');
+    character.style.top = "10px";
+    this.style.height = "400px";
+    this.style.bottom = "0px";
+    document.querySelectorAll('.cardTitle').forEach(t => {
+      t.classList.remove('glass', 'expanded');
+    });
+    cards.forEach(function(c) {
+      c.classList.remove('wait');
+    });
+    title.style.opacity = "100";
+    title.classList.add('glass', 'expanded');
+    };
   });
 });
 
+
+
 async function glideBack(){
-    document.getElementById("arrow").classList.remove('arrow-animation');
-    document.getElementById("arrow").classList.add('arrowBack-animation');
+    aboutPageBackButton.classList.remove('arrow-animation');
+    aboutPageBackButton.classList.add('arrowBack-animation');
     const originalOrder = Array.from(aboutItems);
     for (const element of aboutItems.reverse()) {
       element.classList.remove('glide-animation');
@@ -60,12 +84,15 @@ async function glideBack(){
     menuItems.classList.add('bounceBack-animation');
     cards.forEach(function() {
       cards.forEach(function(c) {
-      var character = c.querySelector('.character');
-      character.style.top = "15%"
-      c.style.height = "250px"
-      c.style.bottom = "-150px"
-      c.classList.remove('clicked');
+        var character = c.querySelector('.character');
+        character.style.top = "15%"
+        c.style.height = "250px"
+        c.style.bottom = "-150px"
+        c.classList.remove('clicked');
       });
+    document.querySelectorAll('.cardTitle').forEach(t => {
+      t.style.opacity = '0';
+    });
     aboutItems = originalOrder
     });
 };
